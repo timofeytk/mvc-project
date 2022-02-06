cdaaa11840f2d672fca3d74c67972d167ad5840f
Тимур Токтошев <timurtoktosev@MacBook-Air-Timur.local>
2022-02-06 13:43:25 +0600

    Structure ready

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f9d5445ef22a12527fef0e0d54fc34337a1a11f3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f9d5445ef22a12527fef0e0d54fc34337a1a11f3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
